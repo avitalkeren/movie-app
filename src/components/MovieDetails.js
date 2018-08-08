fc66b4eb345432266cc8a0f9ed64324735e1786a
Keren Alice <z_avital@hotmail.com>
2018-08-08 18:57:52 +0300

    k

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -28,10 +28,12 @@ class MovieDetails extends Component {
     console.log(details);
 
     return (
-      <div>
+      <div className="flex-container">
         <h1>{details.name}</h1>
-        <img alt="poster" src={details.image.original} />
-        <div>{details.summary}</div>
+        <div className="flex-container-item">
+        <img alt="poster" src={details.image.original} /></div>
+
+        <div className="flex-container-item" dangerouslySetInnerHTML={{__html: details.summary}}></div>
       </div>
     );
   }
